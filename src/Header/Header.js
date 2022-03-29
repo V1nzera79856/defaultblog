61de7eccc20bd53a936175af8837270205b84217
Slaski <UKS8H1@EMEA.Corp.Local>
2022-03-29 15:04:05 +0200

    small changes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,30 @@
 import "./header.scss"
 
+
 export const Header = () => {
+
     return (
         <>
             <section className="section section__header">
                 <header className="blog__header">
-                    <div className="header__logo">logo</div>
+                    <div className="header__logo">
+
+                    </div>
                     <nav className="header__nav">
                         <ul className="nav__list">
-                            <li className="list__item"><a className="list__link" href="/">link</a></li>
-                            <li className="list__item"><a className="list__link" href="/">link</a></li>
-                            <li className="list__item"><a className="list__link" href="/">link</a></li>
-                            <li className="list__item"><a className="list__link" href="/">link</a></li>
-                            <li className="list__item"><a className="list__link" href="/">link</a></li>
-                            <li className="list__item"><a className="list__link" href="/">link</a></li>
+                            <li className="list__item"><a className="list__link" href="/">Home</a></li>
+                            <li className="list__item"><a className="list__link" href="/">About</a></li>
+                            <li className="list__item"><a className="list__link" href="/">Contact</a></li>
+                            <li className="list__item"><a className="list__link" href="/">Shop</a></li>
+                            <li className="list__item"><a className="list__link" href="/">News</a></li>
+                            <li className="list__item">
+                                <label className="switch">
+                                <input type="checkbox" className="checkbox" />
+                                    <span className="slider round">
+
+                                    </span>
+                                </label>
+                            </li>
                         </ul>
                     </nav>
                 </header>
